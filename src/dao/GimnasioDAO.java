8182e8b87dfc2050b2b17d0f13a84db0696b9bf2
Alba Ruiz de la Vega <ruizdelavegaalba@gmail.com>
2026-06-08 12:04:45 +0200

    Remove comments from GimnasioDAO methods

--- src/dao/GimnasioDAO.java
+++ src/dao/GimnasioDAO.java
@@ -18,7 +18,7 @@ public class GimnasioDAO {
         this.emf = emf;
     }
 
-    // a. Inserción de un gimnasio
+   
     public void insertarGimnasio(Gimnasio gimnasio) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
@@ -27,7 +27,7 @@ public class GimnasioDAO {
         em.close();
     }
 
-    // b. Actualización de cualquier campo de un gimnasio (salvo ID), según su ID
+   
     public void actualizarGimnasio(int id, String nombre, String ciudad, double cuotaMensual) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
@@ -41,7 +41,7 @@ public class GimnasioDAO {
         em.close();
     }
 
-    // c. Borrado de un gimnasio, según su ID
+    
     public void borrarGimnasio(int id) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
@@ -57,7 +57,7 @@ public class GimnasioDAO {
         em.close();
     }
 
-    // d. Obtener los Socio de un gimnasio concreto, según su ID
+   
     public List<Socio> obtenerSocios(int id) {
         EntityManager em = emf.createEntityManager();
         Gimnasio gimnasio = em.find(Gimnasio.class, id);
@@ -67,7 +67,6 @@ public class GimnasioDAO {
         return socios;
     }
 
-    // e. Obtener el número de socios inscritos a cada gimnasio
     public Map<String, Long> obtenerNumeroSociosPorGimnasio() {
         EntityManager em = emf.createEntityManager();
         TypedQuery<Object[]> query = em.createQuery(
@@ -84,7 +83,7 @@ public class GimnasioDAO {
         return res;
     }
 
-    // f. Obtener los Gimnasio con menos de 10 socios
+   
     public List<Gimnasio> obtenerGimnasiosConMenosDe10Socios() {
         EntityManager em = emf.createEntityManager();
         TypedQuery<Gimnasio> query = em.createQuery(
@@ -95,7 +94,7 @@ public class GimnasioDAO {
         return gimnasios;
     }
 
-    // g. Obtener los 5 Gimnasio con la cuota más alta
+    
     public List<Gimnasio> obtenerTop5CuotasMasAltas() {
         EntityManager em = emf.createEntityManager();
         TypedQuery<Gimnasio> query = em.createQuery(
@@ -107,7 +106,7 @@ public class GimnasioDAO {
         return gimnasios;
     }
 
-    // h. Obtener el Gimnasio de una ciudad (dada por parámetro) con la cuota más barata
+    
     public Gimnasio obtenerGimnasioMasBaratoPorCiudad(String ciudad) {
         EntityManager em = emf.createEntityManager();
         TypedQuery<Gimnasio> query = em.createQuery(
